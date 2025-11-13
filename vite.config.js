f10d813c60d75f13c7207b846fbc2c4dbad97ba1
snitchlazyrus-collab <snitchlazyrus@gmail.com>
2025-11-13 09:02:01 +0800

    nth commit lmao

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Make sure this is set to '/'
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
